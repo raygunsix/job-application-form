faa180068cbe59432099f16c8e6ca56e71763dc8
Chris Reid <chris@raygun6.com>
2024-12-23 15:51:00 -0800

    message the user on successful form submission

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -30,6 +30,8 @@ def index():
                     email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        
+        flash("Your form was submitted successfully!", "success")
 
     return render_template("index.html")
 
