f0f8c5f0fe7ed6ff4687bacebb3fb08dd80ab371
Chris Reid <chris@raygun6.com>
2024-12-23 15:26:27 -0800

    add database and sqlalchemy

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "myapplication123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -10,8 +23,10 @@ def index():
         email = request.form["email"]
         date = request.form["date"]
         occupation = request.form["occupation"]
-        
+
     return render_template("index.html")
 
-
-app.run(debug=True, port=5001)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
